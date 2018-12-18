1a7dba345a0d7f220b03847839670a91377e1635
ugiriwabo <ugiraconsole@gmail.com>
2018-12-18 14:26:52 +0200

    modify the java script

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -4,18 +4,19 @@ function triangleTracker(){
     var a = parseInt(document.getElementById("a").value);
     var b = parseInt(document.getElementById("b").value);
     var c = parseInt(document.getElementById("c").value);
+    var tracker= ['Equilateral Triangle','Isosceles Triangle','This is not a triangle','Scalene Triangle']
 
         if (a ===b && b === c && c === a) {
-          alert("Equilateral Triangle");
+          alert(tracker[0]);
       }
         else if (a === b || a === c || b === c ) {
-          alert("Isosceles Triangle");
+          alert(tracker[1]);
       }
         else if ((a+b)<=c || (a+c)<=b || (b+c)<=a) {
-          alert("This is not a triangle");
+          alert(tracker[2]);
       }
         else if (a !== b && b !== c ) {
-          alert("Scalene Triangle");
+          alert(tracker[3]);
       }
 
     }
